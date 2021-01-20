3582eb178afe6e8180bfae14393d793cb0391b66
Herve Desrosiers <hdesrosiers@stingray.com>
2021-01-20 14:20:47 -0500

    material components with Scaffold: TopAppBar

--- app/src/main/java/com/hdesrosiers/composelayoutscodelab/MainActivity.kt
+++ app/src/main/java/com/hdesrosiers/composelayoutscodelab/MainActivity.kt
@@ -2,12 +2,27 @@ package com.hdesrosiers.composelayoutscodelab
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Providers
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hdesrosiers.composelayoutscodelab.ui.theme.ComposeLayoutsCodelabTheme
 
 class MainActivity : AppCompatActivity() {
@@ -15,24 +30,102 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                PhotographCard()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LayoutsCodelab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Layouts Codelab"
+                    )
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething()*/ }) {
+                        Icon(Icons.Filled.Favorite)
+                    }
+                }
+            )
+        }
+    ) {
+        innerPadding ->
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(
+            text = "Hi there!",
+        )
+        Text(
+            text = "Thanks for going through the Layouts codelab",
+        )
+    }
+}
+
+@Composable
+fun PhotographCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = { /* Ignoring onClick */ })
+            .padding(16.dp)
+    ) {
+        Surface(
+                modifier = Modifier
+                    .preferredSize(50.dp),
+                shape = CircleShape,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(
+                    text = "Alfred Sisley",
+                    fontWeight = FontWeight.Bold
+            )
+            Providers(AmbientContentAlpha provides ContentAlpha.medium) {
+                Text(
+                        text = "3 minutes ago",
+                        style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun PhotographerCardPreview() {
     ComposeLayoutsCodelabTheme {
-        Greeting("Android")
+        PhotographCard()
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun LayoutsCodelabPreview() {
+    ComposeLayoutsCodelabTheme {
+        LayoutsCodelab()
     }
 }
